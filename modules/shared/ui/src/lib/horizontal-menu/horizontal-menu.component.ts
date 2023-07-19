8266ae253e10aac3e34899a9bae783ab0c436453
Tim Geerts <tim.geerts@sdworx.com>
2023-07-19 12:39:32 +0200

    remove funny home button

--- modules/shared/ui/src/lib/horizontal-menu/horizontal-menu.component.ts
+++ modules/shared/ui/src/lib/horizontal-menu/horizontal-menu.component.ts
@@ -1,8 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { IMenuItem } from '../types/menu-item';
-import { Subscription, filter, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'wivipro-horizontal-menu',
@@ -11,30 +10,8 @@ import { Subscription, filter, shareReplay } from 'rxjs';
   templateUrl: './horizontal-menu.component.html',
   styleUrls: ['./horizontal-menu.component.scss'],
 })
-export class HorizontalMenuComponent implements OnInit, OnDestroy {
+export class HorizontalMenuComponent {
   @Input() class = '';
-  @Input() showHome = false;
   @Input() right = false;
   @Input() menuItems: IMenuItem[] = new Array<IMenuItem>();
-
-  routerSub?: Subscription;
-
-  constructor(private router: Router) {}
-
-  ngOnInit(): void {
-    if (!this.right) {
-      this.routerSub = this.router.events
-        .pipe(
-          filter((e) => e instanceof NavigationEnd),
-          shareReplay({ bufferSize: 1, refCount: true })
-        )
-        .subscribe((r: any) => {
-          this.showHome = r.url !== '/home';
-        });
-    }
-  }
-
-  ngOnDestroy(): void {
-    if (this.routerSub) this.routerSub.unsubscribe();
-  }
 }
