8a4078e5379b14374e2cbc0590e5487b7411e38b
Tim Geerts <tim.geerts1@gmail.com>
2025-01-23 14:29:45 +0100

    pensieon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,18 +7,23 @@ export const appRoutes: Route[] = [
     redirectTo: 'home',
   },
   {
+    path: 'gallery',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
     path: 'home',
     loadComponent: () =>
       import('@wivipro/modules/home').then((m) => m.HomeComponent),
   },
-  {
-    path: 'gallery',
-    loadComponent: () =>
-      import('@wivipro/modules/gallery').then((m) => m.GalleryComponent),
-  },
-  {
-    path: 'admin',
-    loadChildren: () =>
-      import('@wivipro/modules/admin').then((mod) => mod.ADMIN_ROUTES),
-  },
+  // {
+  //   path: 'gallery',
+  //   loadComponent: () =>
+  //     import('@wivipro/modules/gallery').then((m) => m.GalleryComponent),
+  // },
+  // {
+  //   path: 'admin',
+  //   loadChildren: () =>
+  //     import('@wivipro/modules/admin').then((mod) => mod.ADMIN_ROUTES),
+  // },
 ];
